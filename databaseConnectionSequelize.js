661e51f44a759ab307881b1cf718651633bfdb9e
Peter <Peter.Senyk@gmail.com>
2025-03-14 10:20:09 -0700

    UPDATE db connection

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+console.log("Loaded DB Config:", process.env);
+
 const sequelize = new Sequelize({
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
